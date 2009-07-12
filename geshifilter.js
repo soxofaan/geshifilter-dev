e8ffeeb5ae988115762e03f5f5d433816eaea133
Stefaan Lippens <stefaan.lippens+git@gmail.com>
2009-07-12 14:19:18 +0200

    Only do the collapsing when collapse height is smaller than original.

--- geshifilter.js
+++ geshifilter.js
@@ -1,15 +1,20 @@
-var geshifilter_collapse_height = "100px";
+var geshifilter_collapse_height = 100;
 
 $(document).ready(function() {
   $('div.geshifilter').each(function() {
-    // Add more/less link.
-    $(this).append('<div class="geshifilter-tools"><a href="#" class="geshifilter-tools-toggle">Read more</a></div>');
-
+    // Get code container and its height.
     var code_container = $(this).children(':first');
-    // Store the original heights of the geshifilter divs.
-    code_container.attr('original_height', code_container.height());
-    // Collapse by default.
-    code_container.addClass('geshifilter-collapsed').css("height", geshifilter_collapse_height);
+    var code_container_original_height = code_container.height();
+
+    // Only do the collapsing where it makes sense.
+    if (code_container_original_height > geshifilter_collapse_height) {
+      // Add more/less link.
+      $(this).append('<div class="geshifilter-tools"><a href="#" class="geshifilter-tools-toggle">Read more</a></div>');
+      // Store the original heights of the geshifilter divs.
+      code_container.attr('original_height', code_container_original_height);
+      // Collapse by default.
+      code_container.addClass('geshifilter-collapsed').css("height", geshifilter_collapse_height);
+    }
   });
 
   // Add handler to read more/collapse link.
