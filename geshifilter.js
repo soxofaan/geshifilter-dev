add33bf396e239efcdb61fed8bd096b0a3c4adca
Stefaan Lippens <stefaan.lippens+git@gmail.com>
2009-07-12 15:11:31 +0200

    moved from jQuery.ready() to Drupal.behaviors.

--- geshifilter.js
+++ geshifilter.js
@@ -1,6 +1,9 @@
-var geshifilter_collapse_height = 100;
 
-$(document).ready(function() {
+// jQuery magic for partial collapsing/showing GeSHi filter code blocks.
+Drupal.behaviors.geshifilterCollapsing = function(context) {
+  // Collapse height.
+  var geshifilter_collapse_height = 100;
+
   $('div.geshifilter').each(function() {
     // Get code container and its height.
     var code_container = $(this);
@@ -9,7 +12,7 @@ $(document).ready(function() {
     // Only do the collapsing where it makes sense.
     if (code_container_original_height > geshifilter_collapse_height) {
       // Add more/less link.
-      $(this).after('<div class="geshifilter-tools"><a href="#" class="geshifilter-tools-toggle">' + Drupal.t('Show more code') + '</a></div>');
+      $(this).after('<div class="geshifilter-tools"><a href="#" class="geshifilter-tools-collapsetoggle">' + Drupal.t('Show more code') + '</a></div>');
       // Store the original heights of the geshifilter divs.
       code_container.attr('original_height', code_container_original_height);
       // Collapse by default.
@@ -18,7 +21,7 @@ $(document).ready(function() {
   });
 
   // Add handler to read more/collapse link.
-  $("a.geshifilter-tools-toggle").click(function() {
+  $("a.geshifilter-tools-collapsetoggle").click(function() {
     // Get code container.
     var code_container = $(this).parent().prev();
 
@@ -37,4 +40,4 @@ $(document).ready(function() {
     return false;
   });
 
-});
+};
